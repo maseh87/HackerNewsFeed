b275bce785e0e1ee14dcc5b647ee8506dfcf635d
Mason <maseh87@gmail.com>
2014-07-07 11:45:12 -0700

    recieving data from hn

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $http) {
+
+  $scope.articles;
+  $http({
+    method: 'GET',
+    url: "https://community-hnify.p.mashape.com/get/best",
+    headers: {
+      "X-Mashape-Key": "ezm6j6ZDTTmshfYpcmqam21hJaXGp1taozKjsnkZZpn9dmHahi"
+    }
+  }).then(function(req) {
+    console.log(req);
+    $scope.articles = req.data.stories;
+  });
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
