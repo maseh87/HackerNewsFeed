21b0682e9e12744c946a1e7e7c610bdc887647c3
Mason <maseh87@gmail.com>
2014-07-07 14:18:48 -0700

    now showing most recent articles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,17 +16,31 @@ angular.module('starter.controllers', [])
   })
   .finally(function () {
     $ionicLoading.hide();
-  })
+  });
   $scope.loadLink = function(link) {
-
     console.log(link);
     $window.open(link, '_self', location='yes');
 
   };
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('FriendsCtrl', function($scope, $http, $ionicLoading, Friends) {
+  $ionicLoading.show({
+    template: '<i class="icon ion-looping"></i>'
+  });
+  $scope.articles;
+  $http({
+    method: 'GET',
+    url: "https://community-hnify.p.mashape.com/get/newest",
+    headers: {
+      "X-Mashape-Key": "ezm6j6ZDTTmshfYpcmqam21hJaXGp1taozKjsnkZZpn9dmHahi"
+    }
+  }).then(function(req) {
+    $scope.articles = req.data.stories;
+  })
+  .finally(function () {
+    $ionicLoading.hide();
+  });
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,18 +1,10 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
 .factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+  var page = function() {
+
+  };
 
   return {
     all: function() {
